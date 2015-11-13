285e2215ec1bce39e78b70220f58db9228f7cfd4
brunoaosantos <bruno_scp5@hotmail.com>
2015-11-13 10:46:31 +0000

    deleted main from Paragraph class

--- Paragraph.java
+++ Paragraph.java
@@ -1,4 +1,7 @@
 import java.io.*;
+
+
+
 public class Paragraph {
 	
 	private String _text;
@@ -17,16 +20,5 @@ public class Paragraph {
 
 	public int getSize() {
 		return _text.length();
-	}
-
-	public static void main(String[] args) {
-		Paragraph p1 = new Paragraph();
-		Paragraph p2 = new Paragraph();
-		p1.setText("A mae do Afonso é boa!!");
-		p2.setText("Pois é");
-		p2.getSize();
-		p1.getContent();
-		p2.getContent();
-		p1.getSize();
 	}
 }
